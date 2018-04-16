1d669f859a384c6cc0a9b2f5c3e14defdc5825b6
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-04-16 08:17:22 +0300

    service injection

---
+++ di-samples/src/app/cartest/app.component.ts
@@ -0,0 +1,36 @@
+import {CarService} from "./car.service";
+import {Component, Input, OnInit} from "@angular/core";
+
+
+@Component({
+  selector: 'car',
+  template: `
+  <h3>
+    {{ name }} Is Supercharged: {{ supercharged }}
+  </h3>
+  `,
+  styles: [],
+  providers: [CarService]
+})
+
+export class CarComponent implements OnInit {
+  @Input() name;
+  supercharged: string;
+  constructor(private service: CarService){}
+  ngOnInit() {
+    this.supercharged = this.service.isSuperCharged(this.name);
+  }
+}
+
+@Component({
+  selector: 'app.root',
+  template: `
+  <car name="Ford GT"></car>
+  <car name="Corvette ZO6"></car>
+  `,
+  styles:[]
+})
+
+export class AppComponent {
+  title = 'app works!';
+}
